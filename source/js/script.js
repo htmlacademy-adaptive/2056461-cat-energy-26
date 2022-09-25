2eea904b7148319284bacdec8e927d0a10989879
drew666v <drew.vas@yandex.ru>
2022-09-26 00:00:23 +0800

    fix all pages according to perfect pixel and make elements interactive

--- source/js/script.js
+++ source/js/script.js
@@ -1,8 +1,10 @@
 let navToggle = document.querySelector('.main-navigation__toggle');
 let navMain = document.querySelector('.main-navigation__container');
+let footerMap = document.querySelector('.contacts__map');
 
 navToggle.classList.remove('toggle--opened');
 navMain.classList.remove('main-nav--nojs');
+footerMap.classList.remove('map--nojs');
 
 navToggle.addEventListener('click', function () {
   if (navMain.classList.contains('main-navigation--closed')) {
